1036713ed0d89b7bd5cc5572fde60887c64b9b47
DinoProga-coding <134218379+DinoProga-coding@users.noreply.github.com>
2023-07-23 20:13:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function OnClickButton(el){
+    document.getElementById("Language");
+    Language.innerHTML = "Python — это язык программирования, который широко используется в интернет-приложениях и машинном обучении и не только.";
+    Language.style.border =" 4px solid #5eeb34";
+}
+function OnClickButton2(el){
+    document.getElementById("Language");
+    Language.innerHTML = "JavaScript — это полноценный язык программирования, который применяется к HTML документу и обеспечивает интерактив.";
+    Language.style.border =" 4px solid orange";  
+}
+function OnClickButton3(el){
+    document.getElementById("Language");
+    Language.innerHTML = "HTML - стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере.";
+    Language.style.border =" 4px solid yellow";  
+}
+function OnClickButton4(el){
+    document.getElementById("Language");
+    Language.innerHTML = "CSS - язык декорирования и описания внешнего вида документа, написанного с использованием языка разметки HTML ";
+    Language.style.border =" 4px solid rgb(144, 18, 182)";  
+}
+function OnClickSpan (el){
+    alert("В разработке");
+}
+function OnClickSpan2 (el){
+    alert("В разработке");
+}
